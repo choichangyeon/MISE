d6aff1f86ef67750c3a91ed538ae70f95a0dd889
choichangyeon <changyon99@naver.com>
2025-01-13 16:44:48 +0900

    feat:#4-debounce 구현 및 검색결과 화면 추가 구현

--- asset/js/header.js
+++ asset/js/header.js
@@ -1,8 +1,47 @@
 const $search = document.querySelector("#search");
 
-function search(){
-    const keyword = $search.value;
-    console.log(keyword);
+$search.addEventListener("input", debounce(function (e) {
+    console.log(e.target.value);
+    if(e.target.value !== null){
+        searchMoive(e.target.value);
+    }
+}, 200));
+
+function debounce(func, delay) {
+    let timer;
+    return function () {
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    }
 }
 
-search();
+
+async function searchMoive(word) {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`
+        }
+    };
+
+    let res;
+
+    try {
+        res = await fetch(`${API_URL}/search/movie?query=${word}&language=ko&page=1`, options);
+        res = await res.json();
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    console.log(res);
+
+    let movieList = res['results'];
+    appendFlex(movieList);
+
+}
+
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,5 @@
-const $movieFlex = document.querySelector("#movie-flex");
+
+
 
 function getNowPlaying() {
     const options = {
@@ -9,7 +10,7 @@ function getNowPlaying() {
         }
     };
 
-    fetch(`${API_URL}/now_playing?language=ko&page=1&region=KR`, options)
+    fetch(`${API_URL}/movie/now_playing?language=ko&page=1&region=KR`, options)
         .then(res => res.json())
         .then(res => console.log(res))
         .catch(err => console.error(err));
@@ -27,106 +28,21 @@ async function getPopular() {
     let res;
 
     try {
-        res = await fetch(`${API_URL}/popular?language=ko&page=1`, options);
+        res = await fetch(`${API_URL}/movie/popular?language=ko&page=1`, options);
         res = await res.json();
     }
     catch (err) {
         console.error(err);
     }
 
+    console.log(res);
+
     let movieList = res['results'];
-    movieList.forEach(movie => {
 
-        childElement = movieCard(movie);
-
-        if ($movieFlex) {
-            $movieFlex.appendChild(childElement);
-
-        } else {
-            console.error('html 문서오류');
-        }
-
-    });
-
-
+    appendFlex(movieList);
 
     console.log(movieList);
 }
-
-
-
-function movieCard(movie) {
-    let movieName = movie.title;
-    let poster = POSTER_URL + movie.poster_path;
-    let scoreAver = movie.vote_average;
-    let id = movie.id;
-
-    let html = `
-        <div class='movie-card' id=${id}> 
-            <img src="${poster}" alt="포스터 이미지가 존재하지 않습니다.">
-            <h3>${movieName}</h3>
-            ${scoreAver}
-        </div>
-    `;
-
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
-    const childElement = tempElement.firstElementChild;
-
-    childElement.addEventListener("click", function (e) {
-        e.preventDefault();
-        openMovieContent(movie);
-    });
-
-    return childElement;
-
-}
-
-function movieContent(movie) {
-    let movieName = movie.title;
-    let poster = POSTER_URL + movie.poster_path;
-    let scoreAver = movie.vote_average;
-    let releaseDate = movie.release_date;
-    let overview = movie.overview;
-
-    let html = `
-        <dialog id="movie-modal">
-            <h1>${movieName}</h1>
-            <div>
-                <img src="${poster}" alt="포스터 이미지가 존재하지 않습니다.">
-            </div>
-            <div id="bookmarkbtn">
-                <p>
-                    ${overview}
-                </p>
-                ${releaseDate}
-                <br>
-                ${scoreAver}
-            </div>
-            <button id="close-modal">닫기</button>
-        </dialog>
-    `;
-
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
-    const modalElement = tempElement.firstElementChild;
-
-    document.body.appendChild(modalElement);
-   
-    const dialog = document.querySelector("#movie-modal");
-    dialog.showModal();
-
-    const closeButton = document.querySelector("#close-modal");
-    closeButton.addEventListener("click", () => {
-        dialog.close(); 
-        dialog.remove(); 
-    });
-}
-
-function openMovieContent(movie) {
-    movieContent(movie);
-}
-
 
 // getNowPlaying();
 getPopular();

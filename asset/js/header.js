0d6cc71f2e1045663d886e6403d122df445c583b
choichangyeon <changyon99@naver.com>
2025-01-13 10:15:36 +0900

    feat:#4-검색 탭 구현

---
+++ asset/js/header.js
@@ -0,0 +1,8 @@
+const $search = document.querySelector("#search");
+
+function search(){
+    const keyword = $search.value;
+    console.log(keyword);
+}
+
+search();
---
+++ asset/js/information.js
@@ -0,0 +1,4 @@
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTJhZGMyOThkYzY4ZTUzNTZmZTIxOTFmYjBiNzExNyIsIm5iZiI6MTczNjMxNTk3Ny40MzI5OTk4LCJzdWIiOiI2NzdlMTQ0OWYyYzYyMTgwN2RiYjAyZDgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.X50UgxFG-hBsW_81nxM4eW4LGKaeHWaAnJDjAeDfVHw";
+const API_KEY = "c92adc298dc68e5356fe2191fb0b7117";
+const API_URL = "https://api.themoviedb.org/3/movie";
+const POSTER_URL = "https://image.tmdb.org/t/p/w200";

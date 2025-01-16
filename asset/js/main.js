b06ed2a043995328de532c3a83519c317600edf7
choichangyeon <changyon99@naver.com>
2025-01-16 15:31:50 +0900

    feat:#3-hover 효과 추가

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,13 +6,9 @@ import { showmodal } from "./exportfunc.js";
 getPopular();
 //이벤트 위임을 통한 ID 함수
 $movieFlex.addEventListener("click", function(e){
-    
     const card = e.target.closest(".movie-card");
-    
     if (!card) return;
-
     const movieId = card.id;
-
     console.log("클릭한 카드 ID:", movieId);
     showmodal(movieId);
 })
